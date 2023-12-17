72644fb8de88b129ae310245a10a6186c0bc4b87
LucaSforza <lucasforza1234@icloud.com>
2023-12-17 13:23:54 +0100

    added 2 more tests (test_gauss, test_dot_product)

--- tests.py
+++ tests.py
@@ -14,3 +14,17 @@ def test_trasportation_for_non_square_matrix():
     #TODO
     pass
 
+def test_gauss_test1():
+    #TODO: levare matrice che fa questo output di merda
+    expected = [[1, 3, 1, -1], [0.0, -5.0, 3.0, 4.0], [0.0, 0.0, 1.0, 4.0], [0.0, 0.0, 0.0, 1.3999999999999997]]
+    assert expected == gauss(square_matrix)
+    
+def test_dot_product():
+    vector1 = [1,7,3,2]
+    scalar1 = 3
+    expected1 = [3,21,9,6]
+    vector2 = [2,8,4,2,6]
+    scalar2 = 1/2
+    expected2 = [1,4,2,1,3]
+    assert expected1 == dot_product(scalar1,vector1)
+    assert expected2 == dot_product(scalar2,vector2)
