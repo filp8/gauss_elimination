a4ddf87bd893760830014cca8ce9644b14ada671
LucaSforza <lucasforza1234@icloud.com>
2024-01-08 00:19:50 +0100

    First matrix input system

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from gauss import det
+import sys
+import os
 
-# classica matrice quadrata
-square_matrix = [[1,3,1,-1],[3,9,4,1],[2,1,5,2],[0,1,-1,-1]]
+from gauss import printMat
 
-d = det(square_matrix)
-print(f"det: {d}\nMatrix: {square_matrix}")
+def main():
+    matrix: list[list[int]] = []
+    i = 0
+    while True:
+        matrix.append([])
+        line = input("> ")
+        if (line == "e") | (line == "end"):
+            break
+        for literal in line.split():
+            try:
+                matrix[i].append(int(literal))
+            except ValueError:
+                print(f"[INPUT ERROR] '{literal}' is not a base 10 number",file=sys.stderr)
+                os._exit(1)
+        if i == 0:
+            i += 1
+            continue
+        if len(matrix[i - 1]) != len(matrix[i]):
+            print(f"[INPUT ERROR] Matrix lines have different lenghts",file=sys.stderr)
+            os._exit(1)
+        i += 1
+    printMat(matrix)
+
+
+if '__main__' == __name__:
+    main()
