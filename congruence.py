611fe58bc34c450b0baab3ec58b3334c9cf62c17
filp8 <103014078+filp8@users.noreply.github.com>
2024-01-09 21:15:56 +0000

    tavola moltiplicativa con operazione:function in input

--- congruence.py
+++ congruence.py
@@ -10,31 +10,40 @@ def multiplyTable(insieme:list[int])->list[int]:
             table[x].append(insieme[x]*insieme[y])
     return table
 
-somma = lambda x,y:x+y,'+'
+somma = lambda x,y:(x+y,'+')
 moltiplicazione = lambda x,y:(x*y,'*')
 
-def multiplyTableCongruence(insieme:list[int],mod:int,operazione=moltiplicazione)->list[int]:
+def multiplyTableCongruence(insieme:list[int],mod:int,operazione)->list[int]:
     table: list[list[int]] = [[] for _ in range(len(insieme))]
     for x in range(len(insieme)):
         table[x].append(insieme[x])
         for y in range(len(insieme)):
-            table[x].append(getCongruenge(operazione(insieme[x],insieme[y])[0],mod))
-    formatline = [operazione(1,1)[1]]+insieme
+            #print(type(insieme[x]))
+            #print(type(operazione))
+            risultato,segnop = operazione(insieme[x],insieme[y])
+            #print(risultato)
+            table[x].append(getCongruenge(risultato,mod))
+    formatline = [segnop]+insieme
     tableout = [formatline]+table
     return tableout
 
-def generatore(unita:int,operazione=moltiplicazione,limit=int,partenza=0,insPartenza = [])->list[list[int]]:
+def generatore(unita:int,limit:int,insPartenza:list[int]=[],partenza=0,operazione=somma,)->list[list[int]]:
     for i in range(limit):
         insPartenza.append(partenza+i)
     return insPartenza
 
 
 
+print(generatore(1,100))
+
+
 u8 = [1,3,5,7]
 z4 = [0,1,2,3]
 ins1 = [0,1,2,3,4,5,6]
+
+
 printMat(multiplyTable(ins1))
-printMat(multiplyTableCongruence(ins1,7))
+printMat(multiplyTableCongruence(ins1,7,moltiplicazione))
 
 printMat(multiplyTableCongruence(u8,8,somma))
-printMat(multiplyTableCongruence(z4,4))
+printMat(multiplyTableCongruence(z4,4,moltiplicazione))
