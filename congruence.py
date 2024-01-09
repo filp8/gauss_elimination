880822645f58bfdded5a4a5f696d1214706f6727
filp8 <filippo.onofri46@gmail.com>
2024-01-09 01:17:03 +0100

    tavola moltiplicativa per congruenze

--- congruence.py
+++ congruence.py
@@ -10,6 +10,14 @@ def multiplyTable(insieme:list[int])->list[int]:
             table[x].append(insieme[x]*insieme[y])
     return table
 
+def multiplyTableCongruence(insieme:list[int],mod:int)->list[int]:
+    table: list[list[int]] = [[] for _ in range(len(insieme))]
+    for x in range(len(insieme)):
+        for y in range(len(insieme)):
+            table[x].append(getCongruenge(insieme[x]*insieme[y],mod))
+    return table
 
-ins1 = [1,2,3,4,5,6,7,8,9]
+
+ins1 = [0,1,2,3,4,5,6]
 printMat(multiplyTable(ins1))
+printMat(multiplyTableCongruence(ins1,7))
