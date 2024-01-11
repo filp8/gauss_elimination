a8d76c1a95e8678fcedda0a4e6851b28d8fef689
Luca <lucatolomei@icloud.com>
2024-01-11 17:21:57 +0100

    sostituito round dove facevamo la divisione in calculate_scalar con una nuova funzione fractional_division che fa la divisione tramite Fraction

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from gauss import gauss
 from tui import input_matrix,printMat
 
 def main():
-    matrix = input_matrix()
+    #matrix = input_matrix()
+    matrix =[[1, 1, 0, 0],[0, 1, 1, 0],[1, 0, 1, 0],[1, 1, 1, 0]]
     printMat(gauss(matrix)[1])
 
 
